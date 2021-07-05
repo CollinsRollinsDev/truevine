278e8ccd96f23d7281ca36294439115f10a155a3
Collins <collinsrollins07@gmail.com>
2021-07-05 18:14:44 +0100

    fixing errors 3

--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Signup from '../components/AuthFolder/Signup'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Signup from '../components/AuthFolder/Signup';
 
 export default function Home() {
   return (

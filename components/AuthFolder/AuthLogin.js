4f733f588fc1b2ac72a41efa6c2b13fb7c4c8f49
Collins <collinsrollins07@gmail.com>
2021-07-06 19:08:23 +0100

    adding states at authLogin

--- components/AuthFolder/AuthLogin.js
+++ components/AuthFolder/AuthLogin.js
@@ -3,6 +3,10 @@ import { useState } from "react";
 import Link from 'next/link';
 
 const AuthLogin = () => {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
     return (
         <section className={styles.container}>
      <section className={styles.firstChild}>
